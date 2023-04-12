952307bf46d4a7102c1cb36a0504ea8297b05c45
Maryanne Wambugu <2wambugu.marie@gmail.com>
2023-04-13 02:30:53 +0300

    code challenge

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-// Code here
+const beerName = document.getElementById('beer-name');
+beerName.innerHTML = "Oh So Flattening";
+
+const beerImage = document.getElementById("beer-image");
+beerImage.alt = "Oh So Flattening";
+beerImage.src = "https://i.ibb.co/wQ4G0w1/flatiron-brew.png";
+
+const beerDescription = document.getElementById("beer-description");
+beerDescription.innerHTML = "A light, crisp and bitter IPA brewed with English and American hops. A small batch brewed only once.";
+
+let reviews = document.getElementById("review-list");
+reviews.removeChild(reviews.firstChild,reviews.lastChild);
+
+
